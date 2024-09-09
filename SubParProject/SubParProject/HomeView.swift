d084f921a85430dced837b87f628dd211f53eb92
OwenDangel2503 <166428400+dang-el@users.noreply.github.com>
2024-09-09 15:39:12 -0500

    New UI
    Button action still needed. Photo too grainly for final home screen photo

--- SubParProject/SubParProject/HomeView.swift
+++ SubParProject/SubParProject/HomeView.swift
@@ -13,7 +13,14 @@ struct Home: View {
             // Set the background color to green and ignore safe area
         //TODO: make the colors more "green-like"
             //maybe we could apply something above the background to make it look like grass or a hole maybe
-            Color.init(red: 100/255, green: 1, blue: 100/255)
+            
+//            Color.init(red: 130/255, green: 1, blue: 85/255)
+            
+            
+            Image("golf_ball_next_to_hole").resizable()
+                .scaledToFill()
+                .frame(minWidth: 0) // 👈 This will keep other views (like a large text) in the frame
+                            .edgesIgnoringSafeArea(.all)
             
                 .edgesIgnoringSafeArea(.all)
             VStack{
@@ -23,23 +30,21 @@ struct Home: View {
                     Text("SubPar")
                         .bold(true)
                         .font(.system(size: 48))
-                    Image("red_shirt_golfer").resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                        .padding()
-                    Text("Welcome!")
-                        .font(.system(size: 24))
-                    
+                        .offset(x: 0, y: -120)
                 }
-                HStack{
+                HStack(spacing: 130){
                     Button("New User", action: {
                         
                     })
-                    .padding()
+                    
+                    
+                    
                     Button("Returning User", action: {
                         
                     })
-                }
+                }.offset(x:0, y:280)
+                
+                
             }
             
             
